52f33090538d88f5051c67a173151b0a3ccf55bb
Filip Morawski <filipmor92@gmail.com>
2021-03-30 15:00:50 -0300

    porfolio box fix

--- js/loadVideo.js
+++ js/loadVideo.js
@@ -1,6 +1,10 @@
-function loadVideo1() {
-  if (!document.getElementById('image1')) {
-    var videoEl = document.getElementById('video1');
+function loadVideo(videoSource, videoNumber) {
+  portfolio = document.getElementById("portfolio"+String(videoNumber));
+  console.log(portfolio)
+  portfolio.style.display = "none";
+
+  if (!document.getElementById('image' + String(videoNumber))) {
+    var videoEl = document.getElementById('video' + String(videoNumber));
     var playing = !!(videoEl.currentTime > 0 && !videoEl.paused && !videoEl.ended && videoEl.readyState > 2);
     if (playing) {
       videoEl.pause();
@@ -11,16 +15,16 @@ function loadVideo1() {
   }
   else {
     // Replace image with video
-    const main = document.getElementById('image1');
-    const original = document.getElementById('video1');
+    const main = document.getElementById('image' + String(videoNumber));
+    const original = document.getElementById('video' + String(videoNumber));
     const clone = original.cloneNode(true);
     main.replaceWith(clone);
 
     // Play video
-    var videoEl = document.getElementById('video1');
+    var videoEl = document.getElementById('video' + String(videoNumber));
     videoEl.removeAttribute("hidden");
     var sourceEl = videoEl.getElementsByTagName('source')[0];
-    sourceEl.src = '../videos/anomaly_detection.webm';
+    sourceEl.src = videoSource;
     sourceEl.setAttribute("playsinline", "playsinline");
     videoEl.play();
   }
